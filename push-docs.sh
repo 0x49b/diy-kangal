cf9f2592df5c27afd34b2bc54f15439204d876a9
Florian Thiévent <hi@thievent.org>
2022-01-17 21:29:30 +0100

    changed some

--- push-docs.sh
+++ push-docs.sh
@@ -2,9 +2,10 @@
 
 git add docs/
 
-mkdocs build
-git add site/
+# mkdocs build
+# git add site/
 
-git commit -m "updated docs" docs/ site/
+git commit -m "updated docs" docs/ 
+# git commit -m "updated site" site/ 
 
 git push
